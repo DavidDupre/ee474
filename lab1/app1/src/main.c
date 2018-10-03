0bf525978e3f3b5f1079c5c094d918ade2013ffb
Nicholas Orlov <norlov>
2018-10-02 19:39:41 -0700

    Made sure to add changes from last commit

--- lab1/app1/src/main.c
+++ lab1/app1/src/main.c
@@ -7,8 +7,10 @@ int main()
 {
     while (1) {
         printf("\rA B C D");
+        fflush(stdout);
 		for (volatile unsigned int i = 0; i < CYCLES_PER_SEC; i++);
         printf("\r       ");
+        fflush(stdout);
 		for (volatile unsigned int i = 0; i < CYCLES_PER_SEC; i++);
     }
     return 0;
--- lab1/app7/src/main.c
+++ lab1/app7/src/main.c
@@ -7,20 +7,24 @@ const char letters[] = {'A', 'B', 'C', 'D'};
 const unsigned int delays_ms[] = {1000, 2000, 1000, 2000};
 
 void delay (unsigned int delay_ms) {
-    for (volatile unsigned int i = 0; i < CYCLES_PER_MSEC * delay_ms; i++);
+	for (volatile unsigned int i = 0; i < CYCLES_PER_MSEC * delay_ms; i++);
 }
 
 void flash(unsigned int delay_ms, char letter) {
-    printf("\r%c", letter);
-    delay(delay_ms);
+	printf("\r%c", letter);
+	fflush(stdout);
+	delay(delay_ms);
+	printf("\r ");
+	fflush(stdout);
+	delay(delay_ms);
 }
 
 int main()
 {
-    while (1) {
-        for (int j = 0; j < 4; j++) {
-            flash(delays_ms[i], letters[i]);
-        }
-    }
-    return 0;
+	while (1) {
+		for (int j = 0; j < 4; j++) {
+			flash(delays_ms[j], letters[j]);
+		}
+	}
+	return 0;
 }
