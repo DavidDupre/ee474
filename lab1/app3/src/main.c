38d91ba59beff9aba925d9f2cdca08e13344b1e5
DavidDupre <djdupre7@gmail.com>
2018-10-02 22:56:36 -0700

    Conform to coding standards

--- lab1/app3/src/main.c
+++ lab1/app3/src/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const unsigned int CYCLES_PER_MSEC = 480000;
+#define CYCLES_PER_MSEC 480000
+
 
 const char letters[] = {'A', 'B', 'C', 'D'};
 const unsigned int delays_ms[] = {1000, 2000, 1000, 2000};
 
-int main()
-{
+int main() {
     while (1) {
-		for (int j = 0; j < 4; j++) {
-			printf("\r%c", letters[j]);
-			for (volatile unsigned int i = 0; i < CYCLES_PER_MSEC * delays_ms[j]; i++);
-			printf("\r ");
-			for (volatile unsigned int i = 0; i < CYCLES_PER_MSEC * delays_ms[j]; i++);
-		}
+        for (int j = 0; j < 4; j++) {
+            printf("\r%c", letters[j]);
+            for (volatile unsigned int i = 0; i < CYCLES_PER_MSEC * delays_ms[j]; i++);
+            printf("\r ");
+            for (volatile unsigned int i = 0; i < CYCLES_PER_MSEC * delays_ms[j]; i++);
+        }
     }
     return 0;
 }
--- lab1/app7/src/main.c
+++ lab1/app7/src/main.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const unsigned int CYCLES_PER_MSEC = 480000;
+#define CYCLES_PER_MSEC 480000
+
 
 const char letters[] = {'A', 'B', 'C', 'D'};
 const unsigned int delays_ms[] = {1000, 2000, 1000, 2000};
 
-void delay (unsigned int delay_ms) {
-	for (volatile unsigned int i = 0; i < CYCLES_PER_MSEC * delay_ms; i++);
+void delay(unsigned int delay_ms) {
+    for (volatile unsigned int i = 0; i < CYCLES_PER_MSEC * delay_ms; i++);
+    return;
 }
 
 void flash(unsigned int delay_ms, char letter) {
-	printf("\r%c", letter);
-	fflush(stdout);
-	delay(delay_ms);
-	printf("\r ");
-	fflush(stdout);
-	delay(delay_ms);
+    printf("\r%c", letter);
+    fflush(stdout);
+    delay(delay_ms);
+    printf("\r ");
+    fflush(stdout);
+    delay(delay_ms);
+    return;
 }
 
-int main()
-{
-	while (1) {
-		for (int j = 0; j < 4; j++) {
-			flash(delays_ms[j], letters[j]);
-		}
-	}
-	return 0;
+int main() {
+    while (1) {
+        for (int j = 0; j < 4; j++) {
+            flash(delays_ms[j], letters[j]);
+        }
+    }
+    return 0;
 }
