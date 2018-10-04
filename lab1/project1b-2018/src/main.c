64add92d56dd1b3d85aa47206cb42434c626e4bb
White, Philip <pwhite2793@gmail.com>
2018-10-03 17:00:07 -0700

    Added troubleshooting 1

---
+++ lab1/project1b-2018/src/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+//  this is a simple routine that demonstrates how to fill and diaplay an array of characters
+
+// Main was void instead of int, also took parameter void
+int main()
+{
+	int i = 0;									//  declare a working variable
+
+	char myArray[5];							//  declare a character array
+
+	for (i = 0; i <= 5; i++)					//  fill array with characters
+	{
+		//  fill with the ascii characters A..F
+		//  65 is the ascii value for A
+
+		myArray[i]= 65+i;
+	}
+
+	for (i = 0; i <= 5; i++)					//  display the array
+	{
+		printf("%c \n", myArray[i]);
+	}
+
+	printf("\n");
+
+	// Now set to retun 0
+	return 0;
+}
