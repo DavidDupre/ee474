fbce654923dbe7169865621e81e82dd4af83c20b
DavidDupre <djdupre7@gmail.com>
2018-10-03 19:51:50 -0700

    Remove unused flash.h

--- lab1/app8-arduino/flash.h
+++
@@ -1,9 +0,0 @@
-#ifndef flash_h
-#define flash_h
-
-void myDelay(unsigned int *delay_ms);
-
-#include <Elegoo_GFX.h>    // Core graphics library
-#include <Elegoo_TFTLCD.h> // Hardware-specific library
-
-#endif
