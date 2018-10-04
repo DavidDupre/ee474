4503d602fd937e0a54fce4be13e6a6f23cc29279
White, Philip <pwhite2793@gmail.com>
2018-10-03 18:07:44 -0700

    Modified 7 and 8 to use pointers and added troubleshoot 2

--- lab1/app7/src/main.c
+++ lab1/app7/src/main.c
@@ -7,12 +7,12 @@
 const char letters[] = {'A', 'B', 'C', 'D'};
 const unsigned int delays_ms[] = {1000, 2000, 1000, 2000};
 
-void delay(unsigned int delay_ms) {
-    for (volatile unsigned int i = 0; i < CYCLES_PER_MSEC * delay_ms; i++);
+void delay(const unsigned int *delay_ms) {
+    for (volatile unsigned int i = 0; i < CYCLES_PER_MSEC * *delay_ms; i++);
     return;
 }
 
-void flash(unsigned int delay_ms, char letter) {
+void flash(const unsigned int* delay_ms, char letter) {
     printf("\r%c", letter);
     fflush(stdout);
     delay(delay_ms);
@@ -25,7 +25,7 @@ void flash(unsigned int delay_ms, char letter) {
 int main() {
     while (1) {
         for (int j = 0; j < 4; j++) {
-            flash(delays_ms[j], letters[j]);
+            flash(&delays_ms[j], letters[j]);
         }
     }
     return 0;
--- lab1/app8/src/main.c
+++ lab1/app8/src/main.c
@@ -7,7 +7,7 @@ const unsigned int delays_ms[] = {1000, 2000, 1000, 2000};
 int main() {
     while (1) {
         for (int j = 0; j < 4; j++) {
-            flash(delays_ms[j], letters[j]);
+            flash(&delays_ms[j], letters[j]);
         }
     }
     return 0;
