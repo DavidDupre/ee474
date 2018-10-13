b931fdae534131671ebcbc9a47edcc19c6f8b87d
White, Philip <pwhite2793@gmail.com>
2018-10-13 14:51:05 -0700

    fixed console display, added colors, added tft

--- project2/consoleDisplay.ino
+++ project2/consoleDisplay.ino
@@ -1,12 +1,13 @@
 #include <stdint.h>
 #include "consoleDisplay.h"
+#include "colors.h"
 
 void consoleDisplay(void *consoleDisplayData) {
 
     // Cast from void to correct type
     ConsoleDisplayData *data = (ConsoleDisplayData *) consoleDisplayData;
     tft.setCursor(0,30);
-    Clear bottom part of screen without warnings
+    // Clear bottom part of screen without warnings
     tft.fillRect(0,30,300,500,BLACK);
     tft.print("Solar panel is ");
     if (*(data->solarPanelState)) {
@@ -16,7 +17,7 @@ void consoleDisplay(void *consoleDisplayData) {
     }
 
     tft.print("Battery Level: ");
-    tft.print(*(data->batterLevel));
+    tft.print(*(data->batteryLevel));
     tft.println("/100");
 
     tft.print("Fuel Level: ");
