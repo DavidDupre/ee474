288ea7b19b8b2886ff10927b5cec26b2de71c705
DavidDupre <djdupre7@gmail.com>
2018-10-14 13:42:08 -0700

    Finish thruster tests

--- project2/thrusterSubsystemTest.ino
+++ project2/thrusterSubsystemTest.ino
@@ -4,7 +4,7 @@
 /*
  * Test the macros that parse a thruster command
  */
-test(thrustersParse) {
+test(thrusterParse) {
     // duration 2 s, magnitude 4, left and up on
     uint16_t cmd = 0b0000001001000101;
     uint8_t duration = THRUSTER_CMD_DUR(cmd);
@@ -19,4 +19,55 @@ test(thrustersParse) {
     assertFalse(right);
     assertTrue(up);
     assertFalse(down);
+}
+
+/*
+ * Test createThrusterCommand
+ */
+test(thrusterCreateCommand) {
+    uint16_t cmd = createThrusterCommand(true, false, true, false, 4, 2);
+    assertEqual(THRUSTER_CMD_DUR(cmd), 2);
+    assertEqual(THRUSTER_CMD_MAG(cmd), 4);
+    assertTrue(THRUSTER_CMD_LEFT(cmd));
+    assertFalse(THRUSTER_CMD_RIGHT(cmd));
+    assertTrue(THRUSTER_CMD_UP(cmd));
+    assertFalse(THRUSTER_CMD_DOWN(cmd));
+}
+
+/*
+ * Test thrusterSubsystem
+ */
+test(thrusterSubsystem) {
+    // build the input data struct
+    uint8_t magnitude = 10;
+    uint8_t duration = 2;
+    unsigned int thrusterCommand
+        = createThrusterCommand(true, false, true, false, magnitude, duration);
+    unsigned short fuelLevel = 100;
+    ThrusterSubsystemData data = {
+        &thrusterCommand,
+        &fuelLevel
+    };
+
+    // lower the full fuel so it drains much faster (1 fuel/second)
+    thrusterSubsystemFullFuel = 1000;
+
+    // send the new command
+    thrusterSubsystem((void *) &data);
+
+    // wait for the command to finish
+    delay(2000);
+
+    // call the function again to update the fuel level
+    thrusterSubsystem((void *) &data);
+
+    // check that the fuel level was set to the correct amount
+    // 10 * (2 seconds) * (1 fuel/second) = 10 fuel expended
+    int expectedFuelLevel = 90;
+    assertEqual(expectedFuelLevel, (int) fuelLevel);
+
+    // make sure that the fuel doesn't decrease after the command ends
+    delay(1000);
+    thrusterSubsystem((void *) &data);
+    assertEqual(expectedFuelLevel, (int) fuelLevel);
 }
