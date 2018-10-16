16a512c7741411e5f5c9caad2713cad5c53d1962
Nicholas Orlov <norlov>
2018-10-15 20:12:57 -0700

    Update tests

--- project2/powerSubsystemTest.ino
+++ project2/powerSubsystemTest.ino
@@ -4,6 +4,8 @@
 /* The following testing was done on the MEGA after running the simulation for a "while" ~powerGeneration=100:
 
     Start the satellite and ensure solar panel deploys after decreases below BATTERY_LEVEL_LOW
+    Ensure the proper power generation incrementation would happen
+    Ensure the power generation rate of change would flip when appropriate
     Ensure the battery level never decrements below 0
     Ensure the battery level never increases above 100
 */
