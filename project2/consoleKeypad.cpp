9d9c540c5d71eac00fb469c0d6a7c599c932b9df
DavidDupre <djdupre7@gmail.com>
2018-10-28 14:04:03 -0700

    Add touchscreen

--- project2/consoleKeypad.cpp
+++ project2/consoleKeypad.cpp
@@ -1,9 +1,20 @@
 #include "consoleKeypad.h"
 #include "tft.h"
 #include <Elegoo_GFX.h>
+#include <TouchScreen.h>
+
+// pins for the shield
+#define YP A1
+#define XM A2
+#define YM 7
+#define XP 6
+
+#define MINPRESSURE 10
+#define MAXPRESSURE 1000
 
 Elegoo_GFX_Button buttonInc = Elegoo_GFX_Button();
 Elegoo_GFX_Button buttonDec = Elegoo_GFX_Button();
+TouchScreen touchScreen = TouchScreen(XP, YP, XM, YM, 300);
 
 void consoleKeypadInit() {
     buttonInc.initButton(&tft, 90, 200, 50, 50, 2, RED, WHITE, "+", 4);
@@ -12,10 +23,19 @@ void consoleKeypadInit() {
 
 void consoleKeypad(void *consoleKeypadData) {
     ConsoleKeypadData *data = (ConsoleKeypadData *) consoleKeypadData;
+
+    // draw the buttons
     buttonInc.drawButton();
     buttonDec.drawButton();
+
+    // update the buttons' pressed state based on the touchscreen
+    TSPoint point = touchScreen.getPoint();
+    if (point.z > MINPRESSURE && point.z < MAXPRESSURE) {
+        buttonInc.press(buttonInc.contains(point.x, point.y));
+        buttonDec.press(buttonDec.contains(point.x, point.y));
+    }
+
+    // update the speed commands based on the buttons' pressed state
     *data->driveMotorSpeedInc = buttonInc.isPressed();
     *data->driveMotorSpeedDec = buttonDec.isPressed();
-    Serial.print("pressed? ");
-    Serial.println(buttonInc.isPressed());
 }
