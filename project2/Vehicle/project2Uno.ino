2f0cceabe1e9bd8323c67c19659aa02c9be08e42
White, Philip <pwhite2793@gmail.com>
2018-10-27 17:07:06 -0700

    fixed pull request problems, time, response, indentation

--- project2/Vehicle/project2Uno.ino
+++ project2/Vehicle/project2Uno.ino
@@ -1,8 +1,3 @@
-
-String commandFormat(String comm) {
-    return "A<" + comm + ">";
-}
-
 void setup() {
     Serial.begin(9600);
 }
@@ -11,26 +6,9 @@ void loop() {
     char comm;
     if (Serial.available()) {
         comm = Serial.read();
+        // Do not respond if command is null
         if(comm != '\0') {
-        Serial.println(comm);
+            Serial.println(comm);
         }
-    }    
-    // TODO: Figure out what the timeline of the UNO needs to be
+    }
 }
-
-// if(comm == 'F') {
-//         Serial.println(commandFormat("Forward"));
-//     } else if(comm == 'B') {
-//         Serial.println(commandFormat("Back"));
-//     } else if(comm == 'L') {
-//         Serial.println(commandFormat("Left"));
-//     } else if(comm == 'R') {
-//         Serial.println(commandFormat("Right"));
-//     } else if(comm == 'D') {
-//         Serial.println(commandFormat("Drill down - Start"));
-//     } else if(comm == 'H') {
-//         Serial.println(commandFormat("Drill up - Stop"));
-//     }  else {
-//         Serial.println(commandFormat("ERROR"));
-//     }
-// }
--- project2/vehicleComms.cpp
+++ project2/vehicleComms.cpp
@@ -21,30 +21,14 @@ void vehicleComms(void *vehicleCommsData) {
     // Print command to Serial1 for the Uno to pick up
     Serial1.println(comm);
 
-    char response;
-    // TODO: This it probably not a good way to do an interupt, shouldn't use MILLIS
-    unsigned long time = millis();
     // If the command is \0 there is no command and do nothing
-    if (comm != '\0') {
-        while (1) {
-            if (Serial1.available()) {
-                response = Serial1.read();
-                break;
-            }
-
-            // Took too long to respond
-            if (millis() - time < VCOMMS_TIMEOUT) {
-                response = 'E';
-                break;
-            }
-        }
-
-        *data->response = response;
+    if (Serial1.available()) {
+        *data->response = Serial1.read();
         *data->command = '\0';
-        // Serial.println(response);
     }
+        // Serial.println(response);
     /* TODO: Not sure if this is the proper way to show a response
     * Spec had a longer looking response but it also specifies the
     * type as a char
-    /*
+    */
 }
