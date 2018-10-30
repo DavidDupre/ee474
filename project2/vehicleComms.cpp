3101e95970812fceec4475e4ec1a4c6166a37783
White, Philip <pwhite2793@gmail.com>
2018-10-30 13:45:49 -0700

    unesissary extra variable in vehicle

--- project2/vehicleComms.cpp
+++ project2/vehicleComms.cpp
@@ -39,12 +39,10 @@ void vehicleComms(void *vehicleCommsData) {
 
     // Cast from void to correct type
     VehicleCommsData *data = (VehicleCommsData *) vehicleCommsData;
-
-    char comm = *data->vehicleCommand;
     
     // Print command to Serial1 for the Uno to pick up
     // Do not print if there is no command
-    if (comm != '\0') {
+    if (*data->vehicleCommand != '\0') {
         Serial1.println(comm);
         *data->vehicleCommand = '\0';
     }
