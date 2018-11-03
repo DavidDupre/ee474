4b8710d516ce0445b23c387de446b28e4ed95348
DavidDupre <djdupre7@gmail.com>
2018-11-03 10:49:32 -0700

    Add documentation to newer modules

--- project2/consoleKeypad.h
+++ project2/consoleKeypad.h
@@ -12,8 +12,41 @@ typedef struct {
 } ConsoleKeypadData;
 
 
+/******************************************************************************
+ * name: consoleKeypadInit
+ *
+ * inputs: void
+ *
+ * outputs: void
+ *
+ * description:
+ *  consoleKeypadInit initializes the pins used by the console keypad.
+ *
+ * author: David Dupre
+ *****************************************************************************/
 void consoleKeypadInit();
 
+/******************************************************************************
+ * name: consoleKeypad
+ *
+ * inputs: consoleKeypadData
+ *
+ * outputs: void
+ *
+ * description:
+ *  consoleKeypad queries the state of the solar panel motor speed buttons and
+ *  sets driveMotorSpeedInc and driveMotorSpeedDec to indicate which buttons
+ *  are currently pressed. Both may be pressed at once.
+ *
+ *  If less than two seconds have passes since the last time this function ran
+ *  fully, the function will return immediately.
+ *
+ * psuedocode:
+ *  driveMotorSpeedInc = is increase speed button high?
+ *  driveMotorSpeedDec = is decrease speed button high?
+ *
+ * author: David Dupre
+ *****************************************************************************/
 void consoleKeypad(void *consoleKeypadData);
 
 #endif  /* _CONSOLE_KEYPAD_H_ */
--- project2/solarPanel.h
+++ project2/solarPanel.h
@@ -21,8 +21,66 @@ typedef struct {
 } SolarPanelControlData;
 
 
+/******************************************************************************
+ * name: solarPanelControlInit
+ *
+ * inputs: void
+ *
+ * outputs: void
+ *
+ * description:
+ *  solarPanelControlInit initializes the pins and interrupts used by the
+ *  solar panel and initializes the console keypad.
+ *
+ * author: David Dupre
+ *****************************************************************************/
 void solarPanelControlInit();
 
+/******************************************************************************
+ * name: solarPanelControl
+ *
+ * inputs: solarPanelData
+ *
+ * outputs: void
+ *
+ * description:
+ *  solarPanelControl updates the solar panel motor speed using
+ *  driveMotorSpeedInc and driveMotorSpeedDec. It begins deploying or
+ *  retracting the solar panel based on solarPanelDeploy and solarPanelRetract
+ *  commands.
+ *
+ * psuedocode:
+ *  # update speed
+ *  if driveMotorSpeedInc:
+ *      speed += 5
+ *  if driveMotorSpeedDec:
+ *      speed -= 5
+ *
+ *  # process commands
+ *  if solarPanelDeploy
+ *          and solarPanelState is not deployed or deploying:
+ *      solarPanelState = deploying
+ *      add consoleKeypad to task queue
+ *      solarPanelSpeed = maxSpeed / 2
+ *  else if solarPanelRetract
+ *          and solarPanelState is not retracted or retracting:
+ *      solarPanelState = retracting
+ *      add consoleKeypad to task queue
+ *      solarPanelSpeed = maxSpeed / 2
+ *  solarPanelDeploy = false
+ *  solarPanelRetract = false
+ *
+ *  # output PWM
+ *  if deploying:
+ *      dutyCycle = dutyCycleStopped + speed
+ *  else if retracting:
+ *      dutyCycle = dutyCycleStopped - speed
+ *  else:
+ *      dutyCycle = dutyCycleStopped
+ *  set pin to output PWM at dutyCycle
+ *
+ * author: David Dupre
+ *****************************************************************************/
 void solarPanelControl(void *solarPanelData);
 
 #endif /* _SOLAR_PANEL_H_ */
