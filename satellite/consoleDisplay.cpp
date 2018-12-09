319fab05ad72f7973cf86e5db182feea0ebf9843
White, Philip <pwhite2793@gmail.com>
2018-12-09 13:49:34 -0800

    Display toggles on and off with command id 30

--- satellite/consoleDisplay.cpp
+++ satellite/consoleDisplay.cpp
@@ -4,12 +4,15 @@
 #include "solarPanel.h"
 #include "schedule.h"
 #include "sharedVariables.h"
+#include "comsReceive.h"
 
 #define TEXT_SIZE         2
 #define TEXT_HEIGHT       8  // in multiples of TEXT_SIZE
 #define TEXT_WIDTH        6  // in multiples of TEXT_SIZE
 #define HEIGHT            50 // where the display starts
 #define MAX_VALUE_STR_LEN 20
+
+#define CMDID_CONSOLE_TOGGLE 30
 
 
 TCB consoleDisplayTCB;
@@ -22,7 +25,8 @@ ConsoleDisplayData consoleDisplayData = {
     .powerConsumption = &powerConsumption,
     .powerGeneration = &powerGeneration,
     .batteryLow = &batteryLow,
-    .fuelLow = &fuelLow
+    .fuelLow = &fuelLow,
+    .cStatus = &cStatus,
 };
 
 const char *labels[] = {
@@ -34,6 +38,10 @@ const char *labels[] = {
     "Transport Distance: "
 };
 
+bool handleDisplayToggle(uint8_t *data);
+
+void drawLabels();
+
 void consoleDisplayInit() {
     tcbInit(
         &consoleDisplayTCB,
@@ -43,12 +51,15 @@ void consoleDisplayInit() {
         1
     );
 
-    tft.setCursor(0, HEIGHT);
-    tft.setTextSize(TEXT_SIZE);
-    tft.setTextColor(WHITE);
-    for (unsigned short i = 0; i < sizeof(labels) / sizeof(const char *); i++) {
-        tft.println(labels[i]);
-    }
+    // tft.setCursor(0, HEIGHT);
+    // tft.setTextSize(TEXT_SIZE);
+    // tft.setTextColor(WHITE);
+    // for (unsigned short i = 0; i < sizeof(labels) / sizeof(const char *); i++) {
+    //     tft.println(labels[i]);
+    // }
+    drawLabels();
+
+    comsRxRegisterCallback(CMDID_CONSOLE_TOGGLE, handleDisplayToggle);
 }
 
 void printLabel(const char *label, uint8_t lineNum) {
@@ -73,47 +84,73 @@ void consoleDisplay(void *consoleDisplayData) {
 
     // Cast from void to correct type
     ConsoleDisplayData *data = (ConsoleDisplayData *) consoleDisplayData;
-    tft.setTextSize(TEXT_SIZE);
-    tft.setTextColor(YELLOW);
 
-    uint8_t lineNum = 0;
+    if (*data->cStatus == consoleTurningOff) {
+        tft.fillScreen(BLACK);
+        *data->cStatus = consoleOff;
+    } else if (*data->cStatus == consoleOn) {
+        tft.setTextSize(TEXT_SIZE);
+        tft.setTextColor(YELLOW);
 
-    printLabel(labels[lineNum], lineNum);
-    lineNum++;
-    switch(*(data->solarPanelState)) {
-        case SOLAR_PANEL_DEPLOYED:
-            tft.print(F("deployed"));
-            break;
-        case SOLAR_PANEL_DEPLOYING:
-            tft.print(F("deploying"));
-            break;
-        case SOLAR_PANEL_RETRACTING:
-            tft.print(F("retracting"));
-            break;
-        case SOLAR_PANEL_RETRACTED:
-            tft.print(F("retracted"));
-            break;
+        uint8_t lineNum = 0;
+
+        printLabel(labels[lineNum], lineNum);
+        lineNum++;
+        switch(*(data->solarPanelState)) {
+            case SOLAR_PANEL_DEPLOYED:
+                tft.print(F("deployed"));
+                break;
+            case SOLAR_PANEL_DEPLOYING:
+                tft.print(F("deploying"));
+                break;
+            case SOLAR_PANEL_RETRACTING:
+                tft.print(F("retracting"));
+                break;
+            case SOLAR_PANEL_RETRACTED:
+                tft.print(F("retracted"));
+                break;
+        }
+
+        printLabel(labels[lineNum], lineNum);
+        lineNum++;
+        tft.print(data->batteryLevelPtr[0]);
+        tft.print(F("/36"));
+
+        printLabel(labels[lineNum], lineNum);
+        lineNum++;
+        tft.print(*(data->fuelLevel));
+        tft.print(F("/100"));
+
+        printLabel(labels[lineNum], lineNum);
+        lineNum++;
+        tft.print(*(data->powerConsumption));
+
+        printLabel(labels[lineNum], lineNum);
+        lineNum++;
+        tft.print(*(data->powerGeneration));
+
+        printLabel(labels[lineNum], lineNum);
+        lineNum++;
+        tft.print(data->distanceBufferPtr[0]);
     }
 
-    printLabel(labels[lineNum], lineNum);
-    lineNum++;
-    tft.print(data->batteryLevelPtr[0]);
-    tft.print(F("/36"));
+}
 
-    printLabel(labels[lineNum], lineNum);
-    lineNum++;
-    tft.print(*(data->fuelLevel));
-    tft.print(F("/100"));
+bool handleDisplayToggle(uint8_t *data) {
+    if (cStatus == consoleOn) {
+        cStatus = consoleTurningOff;
+    } else {
+        cStatus = consoleOn;
+        drawLabels();
+    }
+    return true;
+}
 
-    printLabel(labels[lineNum], lineNum);
-    lineNum++;
-    tft.print(*(data->powerConsumption));
-
-    printLabel(labels[lineNum], lineNum);
-    lineNum++;
-    tft.print(*(data->powerGeneration));
-
-    printLabel(labels[lineNum], lineNum);
-    lineNum++;
-    tft.print(data->distanceBufferPtr[0]);
+void drawLabels() {
+    tft.setCursor(0, HEIGHT);
+    tft.setTextSize(TEXT_SIZE);
+    tft.setTextColor(WHITE);
+    for (unsigned short i = 0; i < sizeof(labels) / sizeof(const char *); i++) {
+        tft.println(labels[i]);
+    }
 }
